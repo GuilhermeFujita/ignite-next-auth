438d7256f431beed9676cd412bc6f8cf0d028212
GuilhermeFujita <fujitaguilherme@gmail.com>
2021-06-06 12:06:58 -0300

    Validating visitor and auth server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { AuthContext } from '../contexts/AuthContext';
 import { parseCookies } from 'nookies';
 import styles from '../styles/home.module.css';
 import { redirect } from 'next/dist/next-server/server/api-utils';
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -31,19 +32,8 @@ export default function Home() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false
-      }
-    }
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {}
   }
-}
+})
